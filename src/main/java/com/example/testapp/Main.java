5fcf10b697cb74b105837d643a6ea62c0091da92
Steve Hannah <steve@weblite.ca>
2026-06-05 09:32:06 -0700

    Set up test app v1.0.20 with appUpdateMode=prompt

--- src/main/java/com/example/testapp/Main.java
+++ src/main/java/com/example/testapp/Main.java
@@ -4,13 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
+
+    private static final String VERSION = "1.0.20";
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("jDeploy Test App v1.0.19");
+            JFrame frame = new JFrame("jDeploy Test App v" + VERSION);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(400, 300);
 
-            JLabel label = new JLabel("Hello from jDeploy Test App!", SwingConstants.CENTER);
+            JLabel label = new JLabel(
+                    "<html><center>Hello from jDeploy Test App!<br>Version " + VERSION
+                            + "</center></html>",
+                    SwingConstants.CENTER);
             label.setFont(new Font("Arial", Font.BOLD, 18));
             frame.add(label);
 
